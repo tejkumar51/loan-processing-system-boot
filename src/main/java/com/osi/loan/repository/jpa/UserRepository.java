d5ab591ed228781b6e27757256840a71ffc5d2f5
tej <tejkumar451@gmail.com>
2017-02-21 16:41:41 +0530

    adding spring JPA CRUD operations and pagination & sorting

--- src/main/java/com/osi/loan/repository/jpa/UserRepository.java
+++ src/main/java/com/osi/loan/repository/jpa/UserRepository.java
@@ -1,9 +1,27 @@
 package com.osi.loan.repository.jpa;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
 
 import com.osi.loan.domain.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
+
+	@Query("SELECT u.userName FROM User u")
+	public List<String> getAllUserNames(Pageable pageable);
+
+	@Query("SELECT u.userName FROM User u")
+	public List<String> getAllUserNames(Sort sort);
+
+	// @Query("SELECT * from User") - Not needed as spring JPA generates the select query by pre-defined keywords - findBy<domain class property name> 
+	public User findByUserName(String userName);
+
+	@Procedure(procedureName = "update_username_bypk")
+	public void updateUserNameById(Integer userId, String userName);
 
 }
--- src/main/java/com/osi/loan/web/controller/UserController.java
+++ src/main/java/com/osi/loan/web/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.osi.loan.web.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +27,6 @@ public class UserController {
 		this.userRepository = userRepository;
 	}
 
-
 	@RequestMapping(value = "/addUser", method = RequestMethod.GET)
 	public String addUser(Model model) {
 		User user = new User();
@@ -33,43 +36,37 @@ public class UserController {
 
 	@RequestMapping(value = "/addUser", method = RequestMethod.POST)
 	public String addUser(User user) {
-		// userService.addUser(user);
 		userRepository.save(user);
 		return "useradded";
 	}
 
-/*	@RequestMapping(value = "/findUser", method = RequestMethod.GET)
+	@RequestMapping(value = "/findUser", method = RequestMethod.GET)
 	public String findUser(Model model) {
 		User user = new User();
-		List<String> userNameList = userService.getAllUserNames();
+		List<String> userNameList = userRepository.getAllUserNames(new PageRequest(1, 5));
+		List<String> userNameListSorted = userRepository.getAllUserNames(new Sort(Sort.Direction.DESC, "email"));
 		model.addAttribute("user", user);
-		model.addAttribute("userNameList", userNameList);
+		model.addAttribute("userNameList", userNameListSorted);
 		return "findUser";
 	}
 
 	@RequestMapping(value = "/findUser", method = RequestMethod.POST)
 	public String findUser(User user, Model model) {
-		User resultUser = userService.findByUserName(user.getUserName());
+		User resultUser = userRepository.findByUserName(user.getUserName());
 		model.addAttribute("user", resultUser);
 		return "showUser";
 	}
 
 	@RequestMapping(value = "/updateordelete", method = RequestMethod.POST, params = "update")
 	public String updateUser(User user) {
-		userService.updateUser(user);
-		return "userupdated";
+		userRepository.updateUserNameById(user.getUserId(), user.getUserName());
+		return "userUpdated";
 	}
 
 	@RequestMapping(value = "/updateordelete", method = RequestMethod.POST, params = "delete")
 	public String deletUser(User user) {
-		userService.deleteUser(user);
+		userRepository.delete(user);
 		return "userUpdated";
 	}
-
-	// @ModelAttribute("userNameList")
-	public List<String> userTypeList() {
-		List<String> userTypeList = userService.getAllUserNames();
-		return userTypeList;
-	}*/
 
 }
